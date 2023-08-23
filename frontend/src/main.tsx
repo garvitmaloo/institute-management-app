154b4a39d1b3a130a1e24ecaa45c9c92c3fb1c41
garvit <garvitmaloo.21@gmail.com>
2023-08-23 16:29:11 +0530

    GraphQL and React join hands

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
 import App from "./App.tsx";
 import "./index.css";
 
+const client = new ApolloClient({
+  uri: "http://localhost:8080/",
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
